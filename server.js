8a602d892f74e0b4a1f5827b10d38f2361ae5522
Keera2361 <udhayakumar2361@gmail.com>
2026-06-11 10:29:41 +0530

    Commit server.js

--- server.js
+++ server.js
@@ -26,7 +26,3 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-});
